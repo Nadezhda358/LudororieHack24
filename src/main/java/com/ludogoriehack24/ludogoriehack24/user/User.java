2509c12a62dfd77fb48347e8a07a0250488f7d66
TodorYanakiev <todoryanakiev395@gmail.com>
2024-04-19 22:54:25 +0300

    ability add, relations

--- src/main/java/com/ludogoriehack24/ludogoriehack24/user/User.java
+++ src/main/java/com/ludogoriehack24/ludogoriehack24/user/User.java
@@ -1,5 +1,6 @@
 package com.ludogoriehack24.ludogoriehack24.user;
 
+import com.ludogoriehack24.ludogoriehack24.abilities.Ability;
 import com.ludogoriehack24.ludogoriehack24.userFriend.UserFriend;
 import jakarta.persistence.*;
 import lombok.*;
@@ -26,5 +27,15 @@ public class User {
     private String workExperience;
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<UserFriend> friends;
+
+    @ManyToMany
+    @JoinTable(name = "ability_user", joinColumns = @JoinColumn(name = "ability_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private List<Ability> userAbilities;
+
+    @ManyToMany
+    @JoinTable(name = "ability_user_needed", joinColumns = @JoinColumn(name = "needed_ability_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private List<Ability> neededAbilities;
     //role
 }
