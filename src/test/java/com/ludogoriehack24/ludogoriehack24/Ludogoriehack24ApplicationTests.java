2b68ed2da73ea09fcbdb3983a19c38d0d2577ee0
Nadezhda Todorova <ntodor358@gmail.com>
2024-04-19 19:59:43 +0300

    initial commit

---
+++ src/test/java/com/ludogoriehack24/ludogoriehack24/Ludogoriehack24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ludogoriehack24.ludogoriehack24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ludogoriehack24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
